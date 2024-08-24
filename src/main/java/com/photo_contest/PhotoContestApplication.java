e0585f51c9eb62350478932940585d36a8800510
DobromirMD <122624182+DobromirMD@users.noreply.github.com>
2024-08-24 10:50:33 +0300

    Add files via upload

---
+++ src/main/java/com/photo_contest/PhotoContestApplication.java
@@ -0,0 +1,13 @@
+package com.photo_contest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoContestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoContestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/photo_contest/PhotoContestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photo_contest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoContestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
