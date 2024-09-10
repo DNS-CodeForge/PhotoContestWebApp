79c59c53894e860a767508611baddb1612f93dbe
svetoslavst <114879175+SvetoslavStoyanov66@users.noreply.github.com>
2024-09-11 00:19:20 +0300

    photo contest detail ui improvement

--- frontend/src/components/ContestDetail/ContestInfo.jsx
+++ frontend/src/components/ContestDetail/ContestInfo.jsx
@@ -1,23 +1,36 @@
-import { Typography, Box, Button} from '@mui/material'
+import { Typography, Box} from '@mui/material'
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+import WatchLaterIcon from '@mui/icons-material/WatchLater';
 
 export default function ContestInfo ({contest}) {
     console.log(contest);
+ const formattedEndDate = new Date(contest.endDate).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
     return (
-        <Box display={"flex"} flexDirection={"column"} textAlign={"left"}>
+        <Box display={"flex"} flexDirection={"column"} textAlign={"center"} height={"30vh"} marginBottom={"-4rem"}>
             <Typography variant='h4'>{contest.title}</Typography>
             <Typography variant='h5'>Category: {contest.category}</Typography>
-            <Typography variant='h5'>Participants count: {contest.participants.length}</Typography>
-            <Typography variant='h5'>Jury count: {contest.jury.length}</Typography>
-               <Button
-                  sx={{maxWidth:'10rem'}}
-                  variant="contained"
-                  color='success'
-                  onClick={() => {
-                    alert('clicked');
-                  }}
-                >
-                  Join Contest
-                </Button>
+             <Box display={"flex"} marginTop={"auto"} justifyContent={"space-evenly"}>
+             <Box display={"flex"} alignItems={"center"} justifyContent={"center"}>
+            <BorderColorIcon sx={{height:"3rem", width: "3rem"}}/>
+           <Typography variant='h5'> Photo reviewers count: {contest.jury.length}</Typography>
+            </Box>
+           <Box display={"flex"} alignItems={"center"}justifyContent={"center"} >
+            <InsertPhotoIcon sx={{height:"3rem", width: "3rem"}}/>
+            <Typography variant='h5' textAlign={"center"}>Submissions count: {contest.photoSubmissions.length}</Typography>
+            </Box>
+            <Box display={"flex"} alignItems={"center"} justifyContent={"center"} >
+            <WatchLaterIcon sx={{height:"3rem", width: "3rem"}}/>
+            <Typography variant='h5' textAlign={"center"}>Ends: {formattedEndDate}</Typography>
+            </Box>
+
+
+             </Box>
+
         </Box>
     );
 }
