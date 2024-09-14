02a4c95b535728ce36713409de019e524201b9d0
DobromirMD <122624182+DobromirMD@users.noreply.github.com>
2024-09-14 16:57:52 +0300

    Update ToolbarMenu.jsx

--- frontend/src/components/Navbar/ToolbarMenu.jsx
+++ frontend/src/components/Navbar/ToolbarMenu.jsx
@@ -9,7 +9,11 @@ import HomeIcon from '@mui/icons-material/Home';
 import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import GroupIcon from '@mui/icons-material/Group';
 import InfoIcon from '@mui/icons-material/Info';
+import CollectionsIcon from '@mui/icons-material/Collections';
+import HowToVoteIcon from '@mui/icons-material/HowToVote';
 import logo from '../../assets/logo.png';
+import { getRoles } from "../../utils/jwtUtils.jsx";
+import { isAuthenticated } from "../../utils/authUtils.jsx";
 
 export default function ToolbarMenu() {
     const navigate = useNavigate();
@@ -95,39 +99,79 @@ export default function ToolbarMenu() {
                     Home
                 </MenuItem>
 
-                <MenuItem
-                    onClick={() => handleNavigation('/contest')}
-                    sx={{
-                        padding: '1rem 1rem',
-                        paddingRight: '3rem',
-                        fontSize: '1rem',
-                        justifyContent: 'flex-start',
-                        '&:hover': {
-                            backgroundColor: 'rgba(211, 84, 36, 0.8)',
-                            color: '#fff',
-                        },
-                    }}
-                >
-                    <EmojiEventsIcon sx={{ mr: 1, color: '#fff' }} />
-                    Contest
-                </MenuItem>
+                {isAuthenticated(localStorage.getItem('accessToken')) && (
+                    <>
+                        <MenuItem
+                            onClick={() => handleNavigation('/contest')}
+                            sx={{
+                                padding: '1rem 1rem',
+                                paddingRight: '3rem',
+                                fontSize: '1rem',
+                                justifyContent: 'flex-start',
+                                '&:hover': {
+                                    backgroundColor: 'rgba(211, 84, 36, 0.8)',
+                                    color: '#fff',
+                                },
+                            }}
+                        >
+                            <EmojiEventsIcon sx={{ mr: 1, color: '#fff' }} />
+                            Contest
+                        </MenuItem>
 
-                <MenuItem
-                    onClick={() => handleNavigation('/organizer')}
-                    sx={{
-                        padding: '1rem 1rem',
-                        paddingRight: '3rem',
-                        fontSize: '1rem',
-                        justifyContent: 'flex-start',
-                        '&:hover': {
-                            backgroundColor: 'rgba(211, 84, 36, 0.8)',
-                            color: '#fff',
-                        },
-                    }}
-                >
-                    <GroupIcon sx={{ mr: 1, color: '#fff' }} />
-                    Organizer
-                </MenuItem>
+                        <MenuItem
+                            onClick={() => handleNavigation('/my-contests')}
+                            sx={{
+                                padding: '1rem 1rem',
+                                paddingRight: '3rem',
+                                fontSize: '1rem',
+                                justifyContent: 'flex-start',
+                                '&:hover': {
+                                    backgroundColor: 'rgba(211, 84, 36, 0.8)',
+                                    color: '#fff',
+                                },
+                            }}
+                        >
+                            <CollectionsIcon sx={{ mr: 1, color: '#fff' }} />
+                            My Contests
+                        </MenuItem>
+
+                        <MenuItem
+                            onClick={() => handleNavigation('/my-submissions')}
+                            sx={{
+                                padding: '1rem 1rem',
+                                paddingRight: '3rem',
+                                fontSize: '1rem',
+                                justifyContent: 'flex-start',
+                                '&:hover': {
+                                    backgroundColor: 'rgba(211, 84, 36, 0.8)',
+                                    color: '#fff',
+                                },
+                            }}
+                        >
+                            <HowToVoteIcon sx={{ mr: 1, color: '#fff' }} />
+                            My Submissions
+                        </MenuItem>
+
+                        {getRoles(localStorage.getItem('accessToken')).includes('ADMIN') && (
+                            <MenuItem
+                                onClick={() => handleNavigation('/organizer')}
+                                sx={{
+                                    padding: '1rem 1rem',
+                                    paddingRight: '3rem',
+                                    fontSize: '1rem',
+                                    justifyContent: 'flex-start',
+                                    '&:hover': {
+                                        backgroundColor: 'rgba(211, 84, 36, 0.8)',
+                                        color: '#fff',
+                                    },
+                                }}
+                            >
+                                <GroupIcon sx={{ mr: 1, color: '#fff' }} />
+                                Organizer
+                            </MenuItem>
+                        )}
+                    </>
+                )}
 
                 <MenuItem
                     onClick={() => handleNavigation('/about')}
